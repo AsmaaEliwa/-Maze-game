5f8f4cffd4a03a2fff8eda48fd1730c6d868b48d
Asmaa Eliwa <asmaagamalelsayed96@gmail.com>
2023-07-02 11:37:27 -0400

    move function still needes to edite

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
-import Example from "./scripts/main"
-import Map from "./scripts/map"
+import Example from "./scripts/main";
+import Map from "./scripts/map";
 document.addEventListener("DOMContentLoaded", () => {
     const main = document.getElementById("main");
-    new Example(main)
+    new Example(main);
 
     const canvas = document.getElementById("mapCanvas");
-    // debugger
-    new Map (canvas)
-})
+
+    new Map (canvas);
+});
+
--- src/scripts/map.js
+++ src/scripts/map.js
@@ -3,19 +3,24 @@ class Map {
     constructor(canvas) {
         this.canvas = canvas;
         this.createMap = this.createMap.bind(this);
-
+        this.moveElement = this.moveElement.bind(this);
+        this.img = new Image();
+        this.img.src = '1.webp';
+        this.img.onload = this.handleImageLoad.bind(this);
+        this.imageX = 160;
+        this.imageY = 0;
         this.createMap();
-
+        document.addEventListener('keydown', this.moveElement);
 
     }
+
+
+    handleImageLoad() {
+        this.createMap();
+      }
     createMap() {
-
-
         const mapcontainer = this.canvas
-        //1 => wall
-        //0=> path
-        // 3=>target 
-        //2 =>start point
+        //1 => wall //0=> path// 3=>target //2 =>start point
         let map = [
             [1, 1, 1, 1, 3, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1],
             [1, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 1],
@@ -34,11 +39,6 @@ class Map {
             [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
 
         ];
-
-
-
-
-
         const ctx = this.canvas.getContext('2d');
         const cellSize = 40;
         for (let i = 0; i < 15; i++) {
@@ -54,42 +54,35 @@ class Map {
                 } else if (cellValue === 1) {
                     ctx.fillStyle = '#ABEED7';
                 } else if (cellValue === 3) {
-                 
-        const img = new Image(); // Create new img element
-        img.addEventListener(
-            "load",
-            () => {
-                ctx.drawImage(img, x, y,40,40); // execute drawImage statements here
-            },
-            false
-        );
-        img.src = "1.webp"; // Set source path
-
-                    // ctx.fillStyle =  'green' ;
-                }
-
+                        ctx.drawImage(this.img, this.imageX,this.imageY, cellSize, cellSize);
+             
+                    }
                 ctx.fillRect(x, y, cellSize, cellSize);
 
-
-
-
-
-
-
-
+            }}};
+           
+        
+              moveElement(event){
+                event.preventDefault();
+                const ctx = this.canvas.getContext('2d');
+                const speed = 10;
+            
+                if (event.key === 'ArrowLeft') {
+                  this.imageX -= speed;
+                } else if (event.key === 'ArrowRight') {
+                  this.imageX += speed;
+                } else if (event.key === 'ArrowUp') {
+                  this.imageY -= speed;
+                } else if (event.key === 'ArrowDown') {
+                  this.imageY += speed;
+                }
+            
+                ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+                ctx.drawImage(this.img, this.imageX, this.imageY, 40, 40);
+                this.createMap();
             }
         }
-
-    }
-
-
-
-
-
-}
-
-
-
+        
 
 export default Map;
 
